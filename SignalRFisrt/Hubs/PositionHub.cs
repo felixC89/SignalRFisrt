704e089b043cde46c3bca7102400e78b0d72c5c5
felix calderon <felix.calderon89@hotmail.com>
2024-02-01 16:28:43 -0600

    Agregar archivos de proyecto.

---
+++ SignalRFisrt/Hubs/PositionHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+namespace SignalRFisrt.Hubs
+{
+    public class PositionHub : Hub
+    {
+        public async Task SendPosition(int left, int top) 
+        {
+            await Clients.Others.SendAsync("ReceivePosition", left, top);
+        }
+    }
+}
